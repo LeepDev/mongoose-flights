8242cce70d0872210cad784e55cbb4258b15482a
LeepDev <hyungy@gmail.com>
2023-07-20 09:14:44 -0500

    Part 3 w/o bonuses

---
+++ controllers/tickets.js
@@ -0,0 +1,26 @@
+const Flight = require('../models/flight');
+const Ticket = require('../models/ticket');
+
+module.exports = {
+  new: newTicket,
+  create
+};
+
+async function newTicket(req, res) {
+    const newTicket = new Ticket();
+    const flight = await Flight.findById(req.params.id);
+    res.render('tickets/new', { title: 'Add Ticket', flight });
+}
+
+async function create(req, res) {
+    try {
+        req.body.flight = req.params.id;
+        await Ticket.create(req.body);
+        // Always redirect after CUDing data
+        res.redirect(`/flights/${req.params.id}`);
+      } catch (err) {
+        // Typically some sort of validation error
+        console.log(err);
+        res.render('tickets/new', { title: 'Add Ticket', errorMsg: err.message });
+      }
+}
