2c8c142176c00fa9c0ffec7cf1b9d87e7f745ade
LeepDev <hyungy@gmail.com>
2023-07-20 10:51:18 -0500

    Part 2 Bonus

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,6 @@
 const Flight = require('../models/flight')
 const Ticket = require('../models/ticket')
+const _ = require('lodash');
 
 module.exports = {
     index,
@@ -11,7 +12,17 @@ module.exports = {
 async function show(req, res) {
   const flight = await Flight.findById(req.params.id).populate('destinations');
   const tickets = await Ticket.find({ flight: flight._id });
-  res.render('flights/show', { title: 'Flight Detail', flight, tickets });
+  flight.destinations = _.sortBy(flight.destinations, 'arrival');
+  const selectList = ['AUS', 'DFW', 'DEV', 'LAX' , 'SAN'];
+  let idx = selectList.findIndex(port => port === flight.airport);
+  if(idx !== -1)
+      selectList.splice(idx, 1);
+  flight.destinations.forEach(destination => {
+    idx = selectList.findIndex(port => port === destination.airport);
+    if(idx !== -1)
+      selectList.splice(idx, 1);
+  });
+  res.render('flights/show', { title: 'Flight Detail', flight, tickets, selectList });
 }
 
 async function index(req, res) {
